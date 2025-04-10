85ee7baaf614b49769d5f8139de27172222a1506
xjucelio <jucelio_santos@hotmail.com>
2025-04-10 23:23:20 +0100

    Add blog and site_setup applications with initial models, views, and templates

---
+++ djangoapp/site_setup/context_processors.py
@@ -0,0 +1,14 @@
+from site_setup.models import SiteSetup
+
+
+def context_processor_example(request):
+    return {
+        'example': 'veio do context processor (example)'
+    }
+
+
+def site_setup(request):
+    setup = SiteSetup.objects.order_by('-id').first()
+    return {
+        'site_setup': setup,
+    }
