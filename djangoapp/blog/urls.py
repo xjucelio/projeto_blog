dc7149c9f7e2b85efdc53e7f4df008395ae7569b
xjucelio <jucelio_santos@hotmail.com>
2025-04-26 22:39:33 +0100

    Implement tag and search functionality: add tag view to filter posts by tags, and create search view to filter posts by title, excerpt, or content.

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,6 +1,5 @@
-# type: ignore
-from blog.views import category, created_by, index, page, post
-from django.urls import path
+from blog.views import category, created_by, index, page, post, search, tag  # type: ignore
+from django.urls import path  # type: ignore
 
 app_name = 'blog'
 
@@ -10,4 +9,6 @@ urlpatterns = [
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
+    path('tag/<slug:slug>/', tag, name='tag'),
+    path('search/', search, name='search'),
 ]
