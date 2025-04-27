3bf3226d8068b341be7c5386d97866c9e209560d
xjucelio <jucelio_santos@hotmail.com>
2025-04-27 22:31:06 +0100

    Remove debug print statement from PostListView's get_context_data method

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -20,7 +20,6 @@ class PostListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(context)
         context.update({
             'page_title': 'Home - ',
         })
